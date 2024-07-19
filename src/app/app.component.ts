60492e8d64359d7609677e008338b5f2936fc9cd
danielDutraDamiao <136611204+danielDutraDamiao@users.noreply.github.com>
2024-07-19 05:40:57 -0300

    Primeiro commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MenuItem } from 'primeng/api';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'rems-soft-front-projeto';
+export class AppComponent implements OnInit {
+
+  items: MenuItem[] | undefined;
+  
+  ngOnInit(): void {
+    this.items = [
+      { label: 'Home', icon: 'pi pi-fw pi-home' },
+      { label: 'Pedidos', icon: 'pi pi-fw pi-calendar' },
+      { label: 'Produtos', icon: 'pi pi-fw pi-pencil' },
+      { label: 'Compradores', icon: 'pi pi-fw pi-file' },
+      { label: 'Fornecedores', icon: 'pi pi-fw pi-cog' }
+  ];
+  }
 }
